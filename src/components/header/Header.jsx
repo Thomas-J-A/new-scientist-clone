4e903d47da23d36e1d95fc725da81158852be261
Thomas Aspbury <tomaspbury@icloud.com>
2021-10-17 19:40:02 +0100

    Add styles to header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,65 +3,84 @@ import React from 'react';
 const Header = () => (
   <header>
     <div>
-      
-      <div>
+
+      <div className="header-top">
         <div>
-          <p>NEWSLETTERS</p>
+          <span>NEWSLETTERS</span>
           <p>Sign up to read our regular email newsletters</p>
         </div>
         <h1>NewScientist</h1>
         <button type="button">SUBSCRIBE AND SAVE 61%</button>
       </div>
 
-      <nav>
-        <ul>
-          <li>
-            <a href="#">News</a>
-          </li>
-          <li>
-            <a href="#">Podcasts</a>
-          </li>
-          <li>
-            <a href="#">Video</a>
-          </li>
-          <li>
-            <a href="#">Technology</a>
-          </li>
-          <li>
-            <a href="#">Space</a>
-          </li>
-          <li>
-            <a href="#">Physics</a>
-          </li>
-          <li>
-            <a href="#">Health</a>
-          </li>
-          <li>
-            <a href="#">More</a>
-          </li>
-          <li>
-            <a href="#">Shop</a>
-          </li>
-          <li>
-            <a href="#">Courses</a>
-          </li>
-          <li>
-            <a href="#">Events</a>
-          </li>
-          <li>
-            <a href="#">Tours</a>
-          </li>
-          <li>
-            <a href="#">Jobs</a>
-          </li>
-          <li>
+      <div className="header-bottom">
+        <nav className="primary-nav">
+          <ul>
+            <li>
+              <a href="#">News</a>
+            </li>
+            <li>
+              <a href="#">Podcasts</a>
+            </li>
+            <li>
+              <a href="#">Video</a>
+            </li>
+            <li>
+              <a href="#">Technology</a>
+            </li>
+            <li id="selected">
+              <a href="#">Space</a>
+            </li>
+            <li>
+              <a href="#">Physics</a>
+            </li>
+            <li>
+              <a href="#">Health</a>
+            </li>
+            <li className="primary-nav-more">
+              <a href="#">More</a>
+              <i className="fas fa-angle-double-down" />
+              <ul className="primary-nav-dropdown">
+                <li>
+                  <a href="#">Mind</a>
+                </li>
+                <li>
+                  <a href="#">Environment</a>
+                </li>
+                <li>
+                  <a href="#">Crosswords</a>
+                </li>
+              </ul>
+            </li>
+            <li className="secondary-link">
+              <a href="#">Shop</a>
+            </li>
+            <li className="secondary-link">
+              <a href="#">Courses</a>
+            </li>
+            <li className="secondary-link">
+              <a href="#">Events</a>
+            </li>
+            <li className="secondary-link">
+              <a href="#">Tours</a>
+            </li>
+            <li className="secondary-link">
+              <a href="#">Jobs</a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="header-tools">
+          <div className="sign-in">
+            <i className="fas fa-user" />
             <a href="#">Sign In</a>
-          </li>
-          <li>
+          </div>
+          <div className="search">
+            <i className="fas fa-search" />
             <a href="#">Search</a>
-          </li>
-        </ul>
-      </nav>
+          </div>
+        </div>
+      </div>
     </div>
 
   </header>
